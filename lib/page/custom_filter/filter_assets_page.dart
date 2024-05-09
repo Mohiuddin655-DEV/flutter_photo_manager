9183dcef9257e05f48a33c71f8a7d849bc565698
Md. Mohi-Uddin <mohiuddin655.developer@gmail.com>
2024-05-09 18:57:33 +0600

    init

---
+++ lib/page/custom_filter/filter_assets_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
+
+import 'image_list.dart';
+
+class FilterAssetsContent extends StatelessWidget {
+  const FilterAssetsContent({
+    super.key,
+    required this.filter,
+  });
+  final CustomFilter filter;
+
+  Future<List<AssetEntity>> getAssets() async {
+    final count = await PhotoManager.getAssetCount(filterOption: filter);
+    if (count == 0) {
+      return [];
+    }
+    final list = await PhotoManager.getAssetListRange(
+      start: 0,
+      end: count,
+      filterOption: filter,
+    );
+    return list;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<AssetEntity>>(
+      future: getAssets(),
+      builder:
+          (BuildContext context, AsyncSnapshot<List<AssetEntity>> snapshot) {
+        if (snapshot.hasData) {
+          return ImageList(list: snapshot.data!);
+        }
+        return const SizedBox();
+      },
+    );
+  }
+}
